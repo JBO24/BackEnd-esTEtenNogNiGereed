53b009d97cf5c6fa8884ae295cfea500d9875045
Unknown <jens_bosman@hotmail.com>
2018-04-06 11:57:23 +0200

    Initial commit

---
+++ src/main/java/be/ordina/boerenkostbe/boerenkost/BoerenkostApplication.java
@@ -0,0 +1,12 @@
+package be.ordina.boerenkostbe.boerenkost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoerenkostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoerenkostApplication.class, args);
+	}
+}
